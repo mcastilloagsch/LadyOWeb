7fcb9274d51c83f0f38fdf098b2c496ab1dea7c8
GianluOdd <practicante_ti1@guiasyscoutschile.cl>
2022-09-14 17:34:31 -0300

    202209141735 - Se modifica la carpeta Controllers/Branches para que las urls de las APIS no esten en formato crudo en los archivos php de esas carpetas

--- Controllers/Branche/update_branche.php
+++ Controllers/Branche/update_branche.php
@@ -54,15 +54,22 @@ require_once '../authorization.php';
   <br><br><br><br><br><br><br><br>
   <h2>Editar Rama</h2>
   <?php
-  function APIGET($ruta){
-    $url = "http://localhost:100/api/Branches/getObject/{token}/";
-    $respuesta = $url . $ruta;
+  function APIGET($token,$id){
+    $file = fopen( '../../bin/urls_api.config', "r");
+    $url = array();
+
+    while (!feof($file)) {
+        $url[] = fgetcsv($file,null,';');
+    }
+    fclose($file);
+    $APIBranchesGetObject = $url[4][1];
+    $respuesta = $APIBranchesGetObject . $token . "/" . $id;
     return $respuesta;
   }
 
-  $var = $_GET['id'];
-
-  $ruta = APIGET($var);
+  $id = $_GET['id'];
+  $token = $_SESSION['user_token'];
+  $ruta = APIGET($token,$id);
   $json = file_get_contents($ruta);
   $datos = json_decode($json,true);
   ?>
