cad44e6d160fd36c54f87a2268473718ba80c23e
julian_ortega_silva <practicante_ti2@guiasyscoutschile.cl>
2022-09-19 15:16:45 -0300

    202209191517 - Se modifica la carpeta Controllers/Structures para que las urls de las APIS no esten en formato crudo en los archivos php de esas carpetas

--- Controllers/Structure/create_structure.php
+++ Controllers/Structure/create_structure.php
@@ -4,15 +4,28 @@ require_once '../authorization.php';
 $name = $_POST['name'];
 $structure_type_id = $_POST['structure_type_id'];
 $parent_id = $_POST['parent_id'];
+$token = $_SESSION['user_token'];
 
-$urlcreate = "http://localhost:100/api/Structures/ObjInsert/{token}";
-$curl = curl_init($urlcreate);
+function APIPOST($token){
+  $file = fopen( '../../bin/urls_api.config', "r");
+  $url = array();
+
+  while (!feof($file)) {
+      $url[] = fgetcsv($file,null,';');
+  }
+  fclose($file);
+  $APIStructuresObjInsert = $url[42][1];
+  $respuesta = $APIStructuresObjInsert . $token;
+  return $respuesta;
+}
+$ruta = APIPOST($token);
+$curl = curl_init($ruta);
 
 $objeto = array(
-    "name" => $name,
-    "structure_type_id" => $structure_type_id,
-    "parent_id" => $parent_id,
-  );
+  "name" => $name,
+  "structure_type_id" => $structure_type_id,
+  "parent_id" => $parent_id,
+);
 
 $jsonDataEncoded = json_encode($objeto);
 curl_setopt($curl, CURLOPT_POST, 1);
--- Controllers/Structure/edit_structure.php
+++ Controllers/Structure/edit_structure.php
@@ -5,24 +5,36 @@ $id = $_POST['id'];
 $name = $_POST['name'];
 $structure_type_id = $_POST['structure_type_id'];
 $parent_id = $_POST['parent_id'];
-
+$token = $_SESSION['user_token'];
 
 $id = intval($id);
 
 $objeto = array(
-    "id" => $id,
-    "name" => $name,
-    "structure_type_id" => $structure_type_id,
-    "parent_id" => $parent_id,
-  );
+  "id" => $id,
+  "name" => $name,
+  "structure_type_id" => $structure_type_id,
+  "parent_id" => $parent_id,
+);
 
-$urlcreate = "http://localhost:100/api/Structures/ObjUpdate/{token}/$id";
-$curl = curl_init($urlcreate);
+function APIPUT($token){
+  $file = fopen( '../../bin/urls_api.config', "r");
+  $url = array();
+  
+  while (!feof($file)) {
+      $url[] = fgetcsv($file,null,';');
+  }
+  fclose($file);
+  $APIStructuresObjUpdate = $url[43][1];
+  $respuesta = $APIStructuresObjUpdate . $token;
+  return $respuesta;
+}
+$ruta = APIPUT($token);
+$curl = curl_init($ruta);
 
 $jsonDataEncoded = json_encode($objeto);
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($objeto));
 $result = curl_exec($curl);
 header("Location: structure.php");
 
