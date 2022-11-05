c48a6c6a5ccc34932132c2261a723b43900834a9
Rodrigo Arenas Andrade <rodrigo.arenas@gmail.com>
2022-11-05 18:31:45 -0300

    no message

--- Controllers/Country/delete_country.php
+++ Controllers/Country/delete_country.php
@@ -3,13 +3,11 @@ require_once '../authorization.php';
 include_once '../../Common/functions.php';
 
 $id = $_POST['IdCountry'];
-$name = $_POST['CountryName'];
 
 $id = intval($id);
 
 $objeto = array(
-  "IdCountry" => $id,
-  "CountryName" => $name
+  "IdCountry" => $id
 );
 
 $result = CURL_DELETE("APICountryObjDelete", $objeto, "Location: country.php");
