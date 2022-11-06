851857d0a9e2506ebd692964d6c4118919d6300b
Rodrigo Arenas Andrade <rodrigo.arenas@gmail.com>
2022-11-05 22:15:18 -0300

    no message

--- Controllers/Country/country.php
+++ Controllers/Country/country.php
@@ -4,12 +4,17 @@ include_once '../../Common/functions.php';
 include_once '../../Common/html_functions.php';
 
 $titulo = "Mantenedores de Paises";
+
 $general_buttons = [
-  ["id" => "new_country.php", "text" => "Agregar"]
+  ["href" => "new_country.php", "text" => "Agregar"]
 ];
+
 $label_items = [ "ID Country","Nombre","Borrado"];
+
 $api_url = "APICountryGetlist";
+
 $keys = [ "IdCountry","CountryName","IsDeleted"];
+
 $item_buttons = [
     ["id" => "edit-button","href" => "update_country.php?IdCountry=", "text" => "Editar"],
     ["id" => "delete-button","href" => "delete_country.php?IdCountry=", "text" => "Eliminar"]
