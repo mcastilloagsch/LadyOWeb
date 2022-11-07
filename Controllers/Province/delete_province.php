532c5688b1312bdce5b867c424898040ff8aca13
Rodrigo Arenas Andrade <rodrigo.arenas@gmail.com>
2022-11-07 11:18:52 -0300

    no message

--- Controllers/Province/delete_province.php
+++ Controllers/Province/delete_province.php
@@ -10,15 +10,6 @@ $objeto = array(
   "IdProvince" => $id
 );
 
-$ruta = APIGET("APIProvinceObjDelete");
-$curl = curl_init($ruta);
-
-$jsonDataEncoded = json_encode($objeto);
-curl_setopt($curl, CURLOPT_URL,$ruta);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($objeto)); 
-$result = curl_exec($curl);
-header("Location: province.php");
+$result = CURL_DELETE("APIProvinceObjDelete", $objeto, "Location: province.php");
 
 ?>
