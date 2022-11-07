f2856a16d7458cc3d29f26a847587184009c8bec
Rodrigo Arenas Andrade <rodrigo.arenas@gmail.com>
2022-11-07 20:26:55 -0300

    no message

--- Controllers/Country/delete_country.php
+++ Controllers/Country/delete_country.php
@@ -2,7 +2,9 @@
 require_once '../authorization.php';
 include_once '../../Common/functions.php';
 
+$id = intval($_POST['IdCountry']
 
+error_log("GOGO -$id-");
 
 $objeto = array(
   "IdCountry" => intval($_POST['IdCountry'])
