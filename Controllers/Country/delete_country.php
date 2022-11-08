4f5177ba53053f2f0ff324a03d8fca7200175d25
Rodrigo Arenas Andrade <rodrigo.arenas@gmail.com>
2022-11-07 21:39:13 -0300

    no message

--- Controllers/Country/delete_country.php
+++ Controllers/Country/delete_country.php
@@ -5,13 +5,15 @@ include_once '../../Common/html_functions.php';
 
 head_html(0);
 echo "<body>\n";
-foreach($_POST as $key => $value){
-  print_debug("GOGO -$key-$value-\n",1);  
-}
-print_debug("_POST",1);
+print_debug("_POST \n",1);
 print_debug(var_dump($_POST),1);
-print_debug("_SESSION",1);
+print_debug("\n");
+rint_debug("_GET \n",1);
+print_debug(var_dump($_GET),1);
+print_debug("\n");
+print_debug("_SESSION \n",1);
 print_debug(var_dump($_SESSION),1);
+print_debug("\n");
 echo "</body>\n";
 /*
 $objeto = array(
