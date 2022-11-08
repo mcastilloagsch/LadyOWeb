70de5b26cebef8e06b0861d606e52913ba65e60e
Rodrigo Arenas Andrade <rodrigo.arenas@gmail.com>
2022-11-08 09:08:12 -0300

    no message

--- Controllers/Country/delete_country.php
+++ Controllers/Country/delete_country.php
@@ -3,25 +3,18 @@ require_once '../authorization.php';
 include_once '../../Common/functions.php';
 include_once '../../Common/html_functions.php';
 
-/*
-head_html(0);
-echo "<body>\n";
-print_debug("SESSION\n",1);
-print_debug(var_dump($_SESSION),1);
-print_debug("\n",1);
-print_debug("POST\n",1);
-print_debug(var_dump($_POST),1);
-print_debug("\n",1);
-print_debug("GET\n",1);
-print_debug($_GET['IdCountry'],1);
-print_debug("\n",1);
-echo "</body>\n";
-*/
-
 $id_get = "IdCountry";
 $objeto = array(
   "IdCountry" => $id_get
 );
+
+$seconds = 5;
+
+head_html(0);
+echo "<body>\n";
+echo "<h2>sleep $seconds </h2>\n"
+echo "</body>\n";
+sleep($seconds);
 
 $result = CURL_PUT("APICountryObjDelete", $objeto, "Location: country.php", "");
 
