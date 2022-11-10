2726dbd5f02555de7f4edc41e35a9f471f6500f2
Rodrigo Arenas Andrade <rodrigo.arenas@gmail.com>
2022-11-10 09:29:03 -0300

    correcciones en Branch

--- Controllers/Branche/create_branche.php
+++ Controllers/Branche/create_branche.php
@@ -5,13 +5,13 @@ include_once '../../Common/functions.php';
 #$token = $_SESSION['user_token'];
 
 $objeto = array(
-  "IdBranch": 0,
-  "BranchName": $_POST['BranchName'];,
-  "UnitName": $_POST['UnitName'],
-  "TeamName": $_POST['TeamName'],
-  "IsDeleted": false,
-  "LastModificationDate": "01/01/1920",
-  "LastModificationPerson": 0
+  "IdBranch" => 0,
+  "BranchName" => $_POST['BranchName'],
+  "UnitName" => $_POST['UnitName'],
+  "TeamName" => $_POST['TeamName'],
+  "IsDeleted" => false,
+  "LastModificationDate" => "01/01/1920",
+  "LastModificationPerson" => 0
 );
 
 $result = CURL_POST("APIBranchesObjInsert", $objeto,"Location: branche.php");
--- Controllers/Country/create_country.php
+++ Controllers/Country/create_country.php
@@ -1,7 +1,6 @@
 <?php
 require_once '../authorization.php';
 include_once '../../Common/functions.php';
-include_once '../../Common/html_functions.php';
 
 $objeto = array(
  "CountryName" => $_POST['CountryName']
