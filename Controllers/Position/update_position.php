8e777bd2af72d519928340df47f814b42e9a193f
GianluOdd <practicante_ti1@guiasyscoutschile.cl>
2022-11-18 16:22:33 -0300

    202218111622 Se completa PositionType con todos los métodos funcionando

--- Controllers/Position/update_position.php
+++ Controllers/Position/update_position.php
@@ -3,18 +3,19 @@ require_once '../authorization.php';
 include_once '../../Common/functions.php';
 include_once '../../Common/html_functions.php';
 
-$id_get = "IdPosition";
+$id_get = "IdPositionType";
 $titulo = "Editar posiciones";
 $items = [
-    [ "for" => "", "type" => "", "name" => "IdPosition", "hidden" => 1],
-    [ "for" => "Nombre", "type" => "text", "name" => "PositionName", "hidden" => 0],
+    [ "for" => "", "type" => "", "name" => "IdPositionType", "hidden" => 1],
+    [ "for" => "Nombre", "type" => "text", "name" => "PositionTypeName", "hidden" => 0],
     [ "for" => "Tipo de estructura", "type" => "number", "name" => "IdStructureType", "hidden" => 0]
 ];
 
-$action = "edit_country.php";
+$action = "edit_position.php";
 $method = "post";
-$back = "edit_position.php";
+$api_url = "APIPositionTypeGetObject";
+$back = "position.php";
 
-controller_update_item_page($id_get, $titulo, $items, $action, $method, $back);
+controller_update_item_page($id_get, $titulo, $items, $action, $method, $api_url, $back);
 
 ?>
