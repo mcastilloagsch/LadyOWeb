fdbca29d35a799558e8342feebd7441b97fa80f7
GianluOdd <practicante_ti1@guiasyscoutschile.cl>
2022-11-25 16:43:02 -0300

    202225111642 Se completa Mantenedor de Region, todos los métodos funcionando

--- Controllers/Region/delete_region.php
+++ Controllers/Region/delete_region.php
@@ -3,8 +3,9 @@ require_once '../authorization.php';
 include_once '../../Common/functions.php';
 
 $objeto = array(
-    "IdRegion" => intval($_GET['IdRegion'])
-  );
+  "IdRegion" => intval($_GET['IdRegion']),
+  "RegionName" => $_GET['RegionName']
+);
 
 $result = CURL_DELETE("APIRegionObjDelete", $objeto, "Location: region.php", "");
 
--- Controllers/Region/edit_region.php
+++ Controllers/Region/edit_region.php
@@ -2,13 +2,12 @@
 require_once '../authorization.php';
 include_once '../../Common/functions.php';
 
-#$token = $_SESSION['user_token'];
-
 $objeto = array(
-    "IdRegion" => intval($_POST['IdRegion']),
-    "RegionName" => $_POST['RegionName'],
-    "OrderSec" => $_POST['OrderSec']
-  );
+  "IdRegion" => intval($_POST['IdRegion']),
+  "RegionName" => $_POST['RegionName'],
+  "OrderSec" => $_POST['OrderSec'],
+  "RegCod" => $_POST['RegCod']
+);
 
 $result = CURL_PUT("APIRegionObjUpdate", $objeto, "Location: region.php", "");
 
